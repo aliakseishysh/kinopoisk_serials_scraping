9cdec2fdb3188a1544d017ddb5ee928e1c6ec726
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-11-02 19:38:28 +0300

    delete download_page, specify imports, unite funks into find_information()

--- scraper/main.py
+++ scraper/main.py
@@ -1,22 +1,16 @@
 from bs4 import BeautifulSoup
-import requests
+from requests import get
 from utils.files import save_to_csv
 
 serials_dict = {
 }
 
-def get_page(url):
-    return requests.get(url)
-
 def parse_page(url):
-    serials_wrapped = []
     serials = []
-    ratings_wrapped = []
     ratings = []
-    serials_wrapped = find_serials(url)
+    serials_wrapped, ratings_wrapped = find_information(url)
     for ser in serials_wrapped:
         serials.append(ser.text)
-    ratings_wrapped = find_ratings(url)
     for rate in ratings_wrapped:
         ratings.append(rate.text)
     return serials, ratings
@@ -27,18 +21,15 @@ def add_serials_to_dict(serials, ratings):
             serials_dict[serials[i]] = ratings[i]
     
 
-def find_serials(url):
-    soup = BeautifulSoup(get_page(url).text, 'html.parser')
-    return soup.findAll('p', class_='selection-film-item-meta__name')
+def find_information(url):
+    page = get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    serials = soup.findAll('p', class_='selection-film-item-meta__name')
+    ratings = soup.findAll('span', class_='rating__value')
+    return serials, ratings
 
-def find_ratings(url):
-    soup = BeautifulSoup(get_page(url).text, 'html.parser')
-    return soup.findAll('span', class_='rating__value')
-
-def main():
+def run_app():
     for i in range(1, 10):
         serials, ratings = parse_page(f'https://www.kinopoisk.ru/popular/films/?page={i}&sort=popularity&quick_filters=serials&tab=all')
         add_serials_to_dict(serials, ratings)
         save_to_csv(serials_dict)
-
-main()
