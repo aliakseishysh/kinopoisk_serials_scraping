df38b197e5cfa87389d7cf4997b85bbfe6cf06a5
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-11-02 20:04:32 +0300

    create write_headers function

--- utils/files.py
+++ utils/files.py
@@ -1,10 +1,14 @@
 from csv import writer
 
 
-def save_to_csv(dictionary):
-    with open('serials.csv', 'w', newline='') as csvfile:
+def write_headers(file_path, first_header, second_header):
+    with open(file_path, 'a', newline='') as csvfile:
         csv_writer = writer(csvfile) 
-        csv_writer.writerow(['Название', 'Рейтинг'])
+        csv_writer.writerow([first_header, second_header])
+
+def save_to_csv(file_path, dictionary):
+    with open(file_path, 'a', newline='') as csvfile:
+        csv_writer = writer(csvfile) 
         for key, value in dictionary.items():
             csv_writer.writerow([key, value])
         
